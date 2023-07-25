680a7b7c03a784c446e616cfe11e7580a23335c7
Soham Mondal <sohammondal81@gmail.com>
2023-07-25 12:04:41 +0530

    first commit

---
+++ src/hotel/management/system/Conn.java
@@ -0,0 +1,19 @@
+package hotel.management.system;
+
+import java.sql.*;
+
+public class Conn {
+
+    Connection c;
+    Statement s;
+
+    Conn() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            c = DriverManager.getConnection("jdbc:mysql://localhost:3307/hotelmanagementsystem", "root", "password");
+            s = c.createStatement();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
